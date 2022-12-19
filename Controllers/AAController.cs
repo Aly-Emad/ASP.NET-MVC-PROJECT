eeb6d902d50656487c9542ea9f218c4a3b173b17
Ali Emad <89393334+Aly-Emad@users.noreply.github.com>
2022-12-19 02:28:20 +0200

    Add files via upload

---
+++ Controllers/AAController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AAController : Controller
+    {
+        // GET: AA
+        public ActionResult Index()
+        {
+
+            AdventureWorks2019Entities db = new AdventureWorks2019Entities();
+            var item = db.Addresses.ToList();
+            List<Address> Addresses = db.Addresses.ToList();
+            return View(item);
+
+        }
+    }
+}
